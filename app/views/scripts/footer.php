617cd63387e29d8d94d8454344a72770031478b9
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-12-27 18:17:18 +0000

    - Cleaned up duplicated code in footer

--- app/views/scripts/footer.php
+++ app/views/scripts/footer.php
@@ -1,16 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Footer</title>
-</head>
-<body>
     <footer class="bg-yellow-600 text-white p-4">
-        Copiright AG & JV 2021
+        Copyright AG & JV 2021
     </footer>
-</body>
-</html>
