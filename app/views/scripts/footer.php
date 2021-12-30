8c0f353f2c3097061f735ce9a5f7c03982f4f541
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-12-30 16:42:05 +0000

    - Enhanced cosmetics including responsiveness to multiple device types

--- app/views/scripts/footer.php
+++ app/views/scripts/footer.php
@@ -1,4 +1,7 @@
 
-    <footer class="bg-yellow-600 text-white p-4">
-        Copyright AG & JV 2021
+    <footer class="mt-4 p-2 inset-x-0 bottom-0 relative w-[96%] mx-auto rounded-t-lg drop-shadow-2xl">
+        <div class="text-center font-overpass text-sm align-middle">
+        <p class="align-middle">Albert Garcia & Joan Vila <span class="font-icons text-md align-middle ml-2 mr-1">copyright</span>2021</p> 
+        </div>
+        
     </footer>
