563ad164c6eef656e9818245c39f3a771b243d4f
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-12-30 17:06:42 +0000

    - Footer fixes for readability

--- app/views/scripts/footer.php
+++ app/views/scripts/footer.php
@@ -1,7 +1,7 @@
 
-    <footer class="mt-4 p-2 inset-x-0 bottom-0 relative w-[96%] mx-auto rounded-t-lg drop-shadow-2xl">
-        <div class="text-center font-overpass text-sm align-middle">
-        <p class="align-middle">Albert Garcia & Joan Vila <span class="font-icons text-md align-middle ml-2 mr-1">copyright</span>2021</p> 
+    <footer class="mt-4 inset-x-0 bottom-0 w-[100%] mx-auto rounded-t-lg drop-shadow-2xl">
+        <div class="text-center font-overpass text-sm align-middle bg-gradient-to-t from-yellow-600 to-transparent h-min">
+        <p class="align-middle p-2 py-4">Albert Garcia & Joan Vila <span class="font-icons text-md align-middle ml-2 mr-1">copyright</span>2021</p> 
         </div>
         
     </footer>
